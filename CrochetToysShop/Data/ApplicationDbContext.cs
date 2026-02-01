7f54984a8e39a24f21cd0e378e4eb9c08e169646
Yoanna <joanna.stoycheva.bg@gmail.com>
2026-02-01 17:36:09 +0200

    Add Identity + ApplicationDbContext

---
+++ CrochetToysShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CrochetToysShop.Models.Entities;
+
+
+namespace CrochetToysShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Toy> Toys { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<OrderRequest> OrderRequests { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+
+
+    }
+}
