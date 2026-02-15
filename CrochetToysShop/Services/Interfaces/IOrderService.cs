5de371fa33cdf33c09e5bfc489d4e09af67c0843
Yoanna <joanna.stoycheva.bg@gmail.com>
2026-02-15 16:36:45 +0200

    Extend IOrderService with admin methods

---
+++ CrochetToysShop/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using CrochetToysShop.Models.ViewModels.Orders;
+
+namespace CrochetToysShop.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<OrderCreateViewModel?> GetOrderModelAsync(int toyId);
+
+        Task<(bool ok, string? error, int toyId)> CreateOrderAsync(OrderCreateViewModel model);
+
+        Task<IEnumerable<OrderAdminListItemViewModel>> GetAllForAdminAsync();
+
+        Task<bool> MarkCompletedAsync(int id);
+    }
+}
