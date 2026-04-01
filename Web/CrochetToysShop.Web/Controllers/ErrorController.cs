6cccf588db677d30df3fd82528df48ff36719199
Yoanna <joanna.stoycheva.bg@gmail.com>
2026-04-01 11:46:14 +0300

    Add comprehensive error handling and custom error pages
    - Created ErrorController with HandleError action for error routing
    - Added custom 404 NotFound page with user-friendly message
    - Added custom 500 ServerError page with support contact info
    - Updated Program.cs middleware to use custom error routing
    - Configured exception handler and status code page middleware
    - All validation already present in ViewModels for all forms

---
+++ Web/CrochetToysShop.Web/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrochetToysShop.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/error/{statusCode}")]
+        public IActionResult HandleError(int statusCode)
+        {
+            return statusCode switch
+            {
+                404 => View("NotFound"),
+                500 => View("ServerError"),
+                _ => View("Error")
+            };
+        }
+
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
--- Web/CrochetToysShop.Web/Program.cs
+++ Web/CrochetToysShop.Web/Program.cs
@@ -195,10 +195,12 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/error/500");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseStatusCodePagesWithRedirects("/error/{0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
